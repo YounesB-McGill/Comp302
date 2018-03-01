4cbc2605c9d53c2696cb01faf15735be67c9c6f8
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2018-03-01 14:28:57 -0500

    Update ClassNotes2.scala

--- ClassNotes2.scala
+++ ClassNotes2.scala
@@ -146,7 +146,85 @@ object ClassNotes2{
     // This is how we call this:
     fact(5, (r) => { println(r) }) // This works IRL!
     
+    // Why woud we ever want to do this?
+    /*
+    Performance is an issue, so we'll have deeply nested recursive calls (need more (~double) stack space)
+    Making func calls and never return means we go deep in the stack and never go back up. Aha!
+    
+    Since we're never coming back, we don't care about the stack! We can do this with a goto construct similar to tail recursion, 
+    so performance is actually good!
+    
+    CPS is used in some intermediate compilers to optimize code we write in a hi level language.
+    */
+    
+    /**
+    Get slope of a line
+    */
+    def getLine(x1: Double, y1: Double, x2: Double, y2: Double) = {
+      val slope = (y2-y1)/(x2-x1)
+      val intercept = y1 - slope * x1
+      (slope, intercept) // return a tuple
+    }
+    
+    // CPS version
+    def getLine(x1: Double, y1: Double, x2: Double, y2: Double, c: (Double, Double) => Unit) = { // We are NOT constructing an object
+      val slope = (y2-y1)/(x2-x1) 
+      val intercept = y1 - slope * x1
+      c(slope, intercept)
+    }
+    
+    // If slope was zero, we'd have an error
+    def getLine(x1: Double, y1: Double, x2: Double, y2: Double, c: (Double, Double) => Unit, e:() => Unit) = {
+      if(x2 - x1 == 0) e() // exception
+      else{ 
+        val slope = (y2-y1)/(x2-x1) 
+        val intercept = y1 - slope * x1
+        c(slope, intercept)
+      }
+    }
+    
+    // Call like this:
+    getLine(1,4,3,8,
+      (s, i) = {println("Slope = " + s + ", Intercept = " + i)} 
+      () => {println("Failure!")}
+    )
+    
+    // Some esoteric langs we'll see at the end will use CPS
+    
+    
+    
+    /*********************************************/
+    
+    // LAMBDA CALCULUS
+    /*
+    λ-calculus developed in 1930's by Alonzo Church to express computation 
+    (contrast to Turning machine, which is mathematically equivalent)
+    How minimal can we get in lang features and syntax?
+    
+    Everything is going to be func creation and application
+    
+    Define a recursively formed language:
+    1) Variable names (assume finite size): By convention we usually have one-letter var names, like a, b, x.
+    2) Lambda λ
+    3) The dot .
+    4) Brackets ()
+    
+    What are lambda terms?
+    1) Variables are lambda terms // base case
+    2) If M is a lambda term, and N is a lambda term, then (M N) is a lambda term // Amplication (recursive)
+    3) If M is a lambda term, and x is a variable, then (λx.M) // func abstraction. This how we create functions
+    
+    eg:
+    x (xy) (λx.y) (λx.x) (λx.(λy.z))
+    
+    Assume left-assoc: MNP = (MN)P
+    and λx.xyz = (λx.(xyz)) // Continue till u hit a bracket
+    
+    */
+    
+    
   } // end outerFunc
+ 
   
   
 }
