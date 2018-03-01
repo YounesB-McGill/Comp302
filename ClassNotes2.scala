f0acd931b2d3702299245fa537023eb021319cfa
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2018-03-01 13:23:56 -0500

    Update ClassNotes2.scala

--- ClassNotes2.scala
+++ ClassNotes2.scala
@@ -1,5 +1,43 @@
 import scala.io.Source
 
 object ClassNotes2{
-
+  
+  // Thu 1 Mar 2018
+  /*
+    In A2, we did eval: ASTNode, env → String
+    
+    But if we want to do recursion, we need to be careful!
+    
+    How do we return an perhaps anonymous function?
+    We need to do both return a String, or an actual function
+    
+    "{{  {'||foo'} }}" // invocation of an anonymous function definition. nArgs not relevant here
+    INNERITEXT + DEFINE → return nothing, so function name is two spaces, but wait!
+    We trim whitespace and end up w "".
+    So let's create a binding from name to body.
+    But we could have many anon fucntions!
+    
+    So let's return TWO things instead: eval(node, envr) → returns (String or function)
+    
+    How to choose? Can't know in advance, so we will always return a pair String, function)
+    
+    Our lang is dynamically typed like Python. We don't know the explicit type of things
+    
+    "{{  {'||foo'}{'||bar'} }}"
+    INNERITEXT + DEFINE + DEFINE
+    
+    We could have chose to make this an error, but we choose to take the last definition we found.
+    
+    
+    "{{  x  {'y||foo'}|{{y}} }}" // space space x space space
+    
+    Definition of x includes y||foo. We'll allow this as "  x  " + "", ie we treat {'y||foo'} as "", and we bind y to "",
+    so {{y}} is foo.
+    
+  */
+  
+  def foo{
+  }
+  
+  
 }
